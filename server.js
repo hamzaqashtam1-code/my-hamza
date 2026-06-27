6fd48b6a873983a446511cbb6142a535e5740815
hamzaqashtam1-code <hamzaqashtam1@gmail.com>
2026-06-28 02:45:01 +0300

    Update DASHBOARD_URL to new admin panel link

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // --- هنا ضع رابط سيرفر الإدارة بعد أن ترفعه على Render ---
-const DASHBOARD_URL = "https://qmc-admin-panel.onrender.com"; 
+const DASHBOARD_URL = "https://qmc-adman.onrender.com"; 
 
 let systemConfig = { restaurantName: "مطاعم أبو يونس", bgImage: "https://images.unsplash.com/photo-1555396273-367ea4eb4db5", themeColor: "#1e4620", availableTables: 12 };
 let categories = ["الكل"];
